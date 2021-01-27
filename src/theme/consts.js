dbf8da9ff488c3f873ac82235b0c188297cd7d5d
domi <dominika.hustinova@kiwi.com>
2021-01-28 00:25:11 +0100

    style: add color constants

--- src/theme/consts.js
+++ src/theme/consts.js
@@ -1,1 +1,10 @@
 // colors
+const PINK = "#ff3e78";
+const BLACK = "#000";
+const WHITE = "#fff";
+const LIGHT_YELLOW = "#fff3d9";
+const DARK_YELLOW = "#ffbd2e";
+const LIGHT_PURPLE = "#777bf0";
+const DARK_PURPLE = "#453df8";
+const LIGHT_GRAY = "#e5e5e5";
+const DARK_GRAY = "#666";
